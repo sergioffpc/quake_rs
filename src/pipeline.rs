aa4cecf5a6908d16aaf75d3ebddde58a47b0c789
Sérgio Carvalho <sergio.carvalho@gympass.com>
2023-05-26 17:18:24 +0100

    refactor pipeline

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -34,40 +34,14 @@ impl EntityPipeline {
             depth_or_array_layers: 1,
         };
 
-        let albedo_texture = device.create_texture(&wgpu::TextureDescriptor {
-            label: None,
-            size: target_size,
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format: config.format,
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::TEXTURE_BINDING,
-            view_formats: &[],
-        });
+        let albedo_texture = Self::create_attachment_texture(device, config.format, target_size);
         let albedo_view = albedo_texture.create_view(&Default::default());
 
-        let normal_texture = device.create_texture(&wgpu::TextureDescriptor {
-            label: None,
-            size: target_size,
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format: config.format,
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::TEXTURE_BINDING,
-            view_formats: &[],
-        });
+        let normal_texture = Self::create_attachment_texture(device, config.format, target_size);
         let normal_view = normal_texture.create_view(&Default::default());
 
-        let depth_texture = device.create_texture(&wgpu::TextureDescriptor {
-            label: None,
-            size: target_size,
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format: wgpu::TextureFormat::Depth32Float,
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::TEXTURE_BINDING,
-            view_formats: &[],
-        });
+        let depth_texture =
+            Self::create_attachment_texture(device, wgpu::TextureFormat::Depth32Float, target_size);
         let depth_view = depth_texture.create_view(&Default::default());
 
         let model_matrix: [[f32; 4]; 4] = Matrix4::identity().into();
@@ -76,28 +50,8 @@ impl EntityPipeline {
             contents: bytemuck::cast_slice(&[model_matrix]),
             usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
         });
-        let model_bind_group_layout =
-            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-                entries: &[wgpu::BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: wgpu::ShaderStages::VERTEX,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Uniform,
-                        has_dynamic_offset: false,
-                        min_binding_size: None,
-                    },
-                    count: None,
-                }],
-                label: None,
-            });
-        let model_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            layout: &model_bind_group_layout,
-            entries: &[wgpu::BindGroupEntry {
-                binding: 0,
-                resource: model_matrix_buffer.as_entire_binding(),
-            }],
-            label: None,
-        });
+        let (model_bind_group, model_bind_group_layout) =
+            Self::create_model_bind_group(device, &model_matrix_buffer);
 
         let texture_bind_group_layout =
             device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
@@ -125,60 +79,8 @@ impl EntityPipeline {
         chained_bind_group_layouts.push(&model_bind_group_layout);
         chained_bind_group_layouts.push(&texture_bind_group_layout);
 
-        let shader = device.create_shader_module(wgpu::include_wgsl!("entity.wgsl"));
-        let render_pipeline_layout =
-            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                label: None,
-                bind_group_layouts: &chained_bind_group_layouts,
-                push_constant_ranges: &[],
-            });
-        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: None,
-            layout: Some(&render_pipeline_layout),
-            vertex: wgpu::VertexState {
-                module: &shader,
-                entry_point: "vs_main",
-                buffers: &[Vertex1XYZ1N1UV::desc()],
-            },
-            fragment: Some(wgpu::FragmentState {
-                module: &shader,
-                entry_point: "fs_main",
-                targets: &[
-                    Some(wgpu::ColorTargetState {
-                        format: config.format,
-                        blend: Some(wgpu::BlendState::REPLACE),
-                        write_mask: wgpu::ColorWrites::ALL,
-                    }),
-                    Some(wgpu::ColorTargetState {
-                        format: config.format,
-                        blend: Some(wgpu::BlendState::REPLACE),
-                        write_mask: wgpu::ColorWrites::ALL,
-                    }),
-                ],
-            }),
-            primitive: wgpu::PrimitiveState {
-                topology: wgpu::PrimitiveTopology::TriangleList,
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Cw,
-                cull_mode: Some(wgpu::Face::Back),
-                polygon_mode: wgpu::PolygonMode::Fill,
-                unclipped_depth: false,
-                conservative: false,
-            },
-            depth_stencil: Some(wgpu::DepthStencilState {
-                format: wgpu::TextureFormat::Depth32Float,
-                depth_write_enabled: true,
-                depth_compare: wgpu::CompareFunction::LessEqual,
-                stencil: wgpu::StencilState::default(),
-                bias: wgpu::DepthBiasState::default(),
-            }),
-            multisample: wgpu::MultisampleState {
-                count: 1,
-                mask: !0,
-                alpha_to_coverage_enabled: false,
-            },
-            multiview: None,
-        });
+        let render_pipeline =
+            Self::create_render_pipeline(device, config.format, &chained_bind_group_layouts);
 
         Self {
             albedo_texture,
@@ -204,34 +106,11 @@ impl EntityPipeline {
         bind_groups: &'a [&'a wgpu::BindGroup],
         entities: &Vec<Entity>,
     ) {
-        let albedo_attachment = wgpu::RenderPassColorAttachment {
-            view: &self.albedo_view,
-            resolve_target: None,
-            ops: wgpu::Operations {
-                load: wgpu::LoadOp::Clear(wgpu::Color::BLUE),
-                store: true,
-            },
-        };
-        let normal_attachment = wgpu::RenderPassColorAttachment {
-            view: &self.normal_view,
-            resolve_target: None,
-            ops: wgpu::Operations {
-                load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
-                store: true,
-            },
-        };
-        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            label: None,
-            color_attachments: &[Some(albedo_attachment), Some(normal_attachment)],
-            depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
-                view: &self.depth_view,
-                depth_ops: Some(wgpu::Operations {
-                    load: wgpu::LoadOp::Clear(1.0),
-                    store: true,
-                }),
-                stencil_ops: None,
-            }),
-        });
+        let albedo_attachment = Self::create_render_pass_color_attachment(&self.albedo_view);
+        let normal_attachment = Self::create_render_pass_color_attachment(&self.normal_view);
+        let color_attachments = [Some(albedo_attachment), Some(normal_attachment)];
+        let render_pass_desc = Self::create_render_pass_desc(&color_attachments, &self.depth_view);
+        let mut render_pass = encoder.begin_render_pass(&render_pass_desc);
 
         render_pass.set_pipeline(&self.render_pipeline);
         for (i, bind_group) in bind_groups.iter().enumerate() {
@@ -267,5 +146,145 @@ impl EntityPipeline {
                 render_pass.draw_indexed(0..mesh_component.index_count, 0, 0..1);
             }
         }
+    }
+
+    fn create_render_pass_color_attachment<'a>(
+        view: &'a wgpu::TextureView,
+    ) -> wgpu::RenderPassColorAttachment {
+        wgpu::RenderPassColorAttachment {
+            view,
+            resolve_target: None,
+            ops: wgpu::Operations {
+                load: wgpu::LoadOp::Clear(wgpu::Color::BLUE),
+                store: true,
+            },
+        }
+    }
+
+    fn create_render_pass_desc<'desc, 'tex>(
+        color_attachments: &'desc [Option<wgpu::RenderPassColorAttachment<'tex>>],
+        depth_view: &'tex wgpu::TextureView,
+    ) -> wgpu::RenderPassDescriptor<'desc, 'tex> {
+        wgpu::RenderPassDescriptor {
+            label: None,
+            color_attachments,
+            depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                view: depth_view,
+                depth_ops: Some(wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(1.0),
+                    store: true,
+                }),
+                stencil_ops: None,
+            }),
+        }
+    }
+
+    fn create_attachment_texture(
+        device: &wgpu::Device,
+        format: wgpu::TextureFormat,
+        size: wgpu::Extent3d,
+    ) -> wgpu::Texture {
+        device.create_texture(&wgpu::TextureDescriptor {
+            label: None,
+            size,
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format,
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::TEXTURE_BINDING,
+            view_formats: &[],
+        })
+    }
+
+    fn create_model_bind_group(
+        device: &wgpu::Device,
+        buffer: &wgpu::Buffer,
+    ) -> (wgpu::BindGroup, wgpu::BindGroupLayout) {
+        let model_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                entries: &[wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::VERTEX,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                }],
+                label: None,
+            });
+        let model_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &model_bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: buffer.as_entire_binding(),
+            }],
+            label: None,
+        });
+
+        (model_bind_group, model_bind_group_layout)
+    }
+
+    fn create_render_pipeline<'a>(
+        device: &wgpu::Device,
+        format: wgpu::TextureFormat,
+        bind_group_layouts: &'a [&'a wgpu::BindGroupLayout],
+    ) -> wgpu::RenderPipeline {
+        let shader = device.create_shader_module(wgpu::include_wgsl!("entity.wgsl"));
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: None,
+                bind_group_layouts,
+                push_constant_ranges: &[],
+            });
+
+        device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: None,
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[Vertex1XYZ1N1UV::desc()],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[
+                    Some(wgpu::ColorTargetState {
+                        format,
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    }),
+                    Some(wgpu::ColorTargetState {
+                        format,
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    }),
+                ],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Cw,
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: wgpu::TextureFormat::Depth32Float,
+                depth_write_enabled: true,
+                depth_compare: wgpu::CompareFunction::LessEqual,
+                stencil: wgpu::StencilState::default(),
+                bias: wgpu::DepthBiasState::default(),
+            }),
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+        })
     }
 }
